59dc7a635cd16781262857e5067eb2fd5e85d265
YamalAli770 <yamalali770@gmail.com>
2022-09-28 10:15:58 +0500

    updated project data

--- src/project-data.js
+++ src/project-data.js
@@ -19,12 +19,11 @@ const projectData = [
     },
     {
         id: 3,
-        title: 'Foodlo',
-        desc: "A ReactJS based recipe application using which users can view multiple recipes. The recipes can be filtered based upon different categories. The recipe page lists the particular recipe's required instructions and ingredients.",
-        note: 'Site currently under development.',
-        img: 'images/Foodlo.png',
-        liveLink: '',
-        githubLink: 'https://github.com/YamalAli770/foodloNodeJS'
+        title: 'Guy That Cooks',
+        desc: "Guythatcooks is a food blog based upon the MERN stack where users could view different food recipes and filter them based upon different cuisines. The users could also favourite a particular blog to access it quickly",
+        img: 'images/guythatcooks.png',
+        liveLink: 'https://guythatcooks.netlify.app/',
+        githubLink: 'https://github.com/YamalAli770/guythatcooks-frontend'
     },
 ]
 
